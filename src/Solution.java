8bcf96007850680889273c5f3cc205a321bb41f2
Ribake Ikumoluyi <reiybake05@googlemail.com>
2018-03-08 00:25:51 +0000

    Initial set up

---
+++ src/Solution.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ribake on 08/03/2018.
+ */
+public class Solution {
+    // remove uncommon characters from both strings
+
+    // remove extraneous common characters from both strings
+}
